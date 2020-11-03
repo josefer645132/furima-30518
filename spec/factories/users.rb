191b494d3c5289906b9dc5c4bdd8ace57b83f976
josefer645132 <yajima.shintaro@gmail.com>
2020-11-04 06:36:09 +0900

    バリデーション追記

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,8 +2,13 @@ FactoryBot.define do
   factory :user do
     nickname              { 'nickname' }
     email                 { 'kkk@gmail.com' }
-    password              { '000000' }
+    password              { '000000'}
     password_confirmation { password }
+    family_name           { 'family_name' }
+    family_name_frigana   { 'family_name_frigana' }
+    first_name            { 'first_name' }
+    first_name_frigana    { 'first_name_frigana' }
+    birthday              { 'birthday' }
     
 
   
