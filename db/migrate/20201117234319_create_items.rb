f8992f95a9d575ff339451b94378a11c17138a93
josefer645132 <yajima.shintaro@gmail.com>
2020-11-18 08:55:32 +0900

    モデル・テーブル作成

---
+++ db/migrate/20201117234319_create_items.rb
@@ -0,0 +1,18 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+
+      t.string  :name, null: false
+      t.text    :explanation, null: false
+      t.integer :category_id, null: false
+      t.integer :status_id, null: false
+      t.integer :shipping_cost_id, null: false
+      t.integer :delivery_area_id, null: false
+      t.integer :days_to_delivery_id, null: false
+      t.integer :price_id, null: false
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
