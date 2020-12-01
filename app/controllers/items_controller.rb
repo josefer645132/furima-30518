a0265f1a030fa1b2b3417adde08369ffcedb639e
josefer645132 <yajima.shintaro@gmail.com>
2020-12-02 07:03:16 +0900

    商品出品ページの表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: :new
+  before_action :authenticate_user!, only: [:new]
   
   
   def index
@@ -7,16 +7,16 @@ class ItemsController < ApplicationController
   end
 
   def create
-    binding.pry
     Item.create(item_params)
   end
-
+  
   def message_params
     params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
   end
-
-
+  
+  
   def new
+    #binding.pry
     @item = Item.new
   end
 
