d5add95f791e7c8ffdb6baf953901246d45daaa5
josefer645132 <yajima.shintaro@gmail.com>
2020-12-17 12:16:41 +0900

    before_actionのコメントアウト及びprivateメソッドの追記、newアクションの改行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,24 +1,26 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  #before_action :authenticate_user!, only: [:new]
   
   
   def index
     @items = Item.all
   end
 
+  def new
+    @item = Item.new
+  end
  
   def create
     @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
     else
-      
-      render :new
+        render :new
     end
   end
 
 
-
+ private
   def item_params
     params.require(:item).permit(:image, :name, :explanation, :category_id, :status_id, :shipping_cost_id, :delivery_area_id, :days_to_delivery_id, :price).merge(user_id: current_user.id)
   end
@@ -30,9 +32,6 @@ class ItemsController < ApplicationController
   end
   
   
-  def new
-
-    @item = Item.new
-  end
+ 
 
 end
