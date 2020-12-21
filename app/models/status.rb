e4aad343f3a1ba6644f8ac1c61235aa5f89eacfb
josefer645132 <yajima.shintaro@gmail.com>
2020-12-21 21:25:55 +0900

    modelsの修正

--- app/models/status.rb
+++ app/models/status.rb
@@ -3,7 +3,7 @@ class Status < ActiveHash::Base
       { id: 0, data: '---' },
       { id: 1, data: '新品・未使用' },
       { id: 2, data: '未使用に近い' },
-      { id: 3, data: '目立った傷や汚れなし' }
+      { id: 3, data: '目立った傷や汚れなし' },
       { id: 4, data: 'やや傷や汚れあり' },
       { id: 5, data: '傷や汚れあり' },
       { id: 6, data: '全体的に状態が悪い' },
