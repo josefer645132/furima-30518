efdef60e0bfe4118e09d57e83c2497a1e8483fc2
josefer645132 <yajima.shintaro@gmail.com>
2020-12-23 20:36:27 +0900

    過去ファイルの誤謬の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
          validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters."}
          validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters."}
          validates :family_name_frigana, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters."}
-         validates :first_name_frigana, format: { with: /\A[0-9]+\z/, message: "Full-width katakana characters."}
+         validates :first_name_frigana, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters."}
          validates :birthday
          validates :nickname
          validates :email
